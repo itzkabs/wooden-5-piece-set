741b7aacc20ace9a98fd6b6dfd2222c4ebd50806
Kabul Hussain Barbhuiya <kabulbarbhuiya@outlook.com>
2025-04-11 14:46:05 -0500

    fix image path

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -8,7 +8,7 @@ const ProductPage = () => {
   const product = {
     title: "Wooden Spoons for Cooking, Wooden Cooking Utensils Set, Nonstick Kitchen Cookware – 5-piece set of wooden utensils with holder",
     price: "$21.99",
-    image: "../images/main.png", // Replace with actual image URL
+    image: "../wooden-5-piece-set/images/main.png", // Replace with actual image URL
     bulletPoints: [
         "UNMATCHED DURABILITY: Crafted from a single piece of 100% natural teak wood, AYZI. kitchen utensils promise a lifetime of use. These tools are not only beautiful but also solid, sturdy, smooth, and well-designed with a superb finish. The spoon set is guaranteed not to crack, melt, or retain odors. Due to the natural material, the utensils may vary in shade",
         "COMPLETE SET OF 5: This all-inclusive and comprehensive set thoughtfully comprises five essential cooking utensils, each meticulously selected and curated to meet the diverse culinary needs of any kitchen enthusiast. The set also boasts a beautifully crafted wooden barrel, which provides ample storage space for all your utensils, ensuring a neat and organized kitchen environment, making every meal preparation a seamless and enjoyable endeavor.",
@@ -20,27 +20,27 @@ const ProductPage = () => {
     reviews: 1,
     galleryImages: [
         {
-          url: "%PUBLIC_URL%/images/2.jpg",
+          url: "/wooden-5-piece-set/images/2.jpg",
           description: "Five Essential Pieces: Includes a variety of spatula shapes and sizes for different cooking tasks—ideal for flipping, stirring, and scraping.",
         },
         {
-            url: "%PUBLIC_URL%/images/3.jpg",
+            url: "/wooden-5-piece-set/images/3.jpg",
           description: "Ergonomic Design: Comfortable, well-balanced handles provide a secure grip for easy maneuverability.",
         },
         {
-            url: "%PUBLIC_URL%/images/4.jpg",
+            url: "/wooden-5-piece-set/images/4.jpg",
           description: "Non-Scratch: Gentle on all cookware, including non-stick pans, preventing scratches and damage.",
         },
         {
-            url: "%PUBLIC_URL%/images/2.jpg",
+            url: "/wooden-5-piece-set/images/2.jpg",
             description: "Premium Quality Material: Made from durable, sustainably sourced teak wood known for its strength and natural resistance to moisture.",
           },
           {
-              url: "%PUBLIC_URL%/images/3.jpg",
+              url: "/wooden-5-piece-set/images/3.jpg",
             description: "Easy to Clean: Hand wash with warm water and mild soap for long-lasting use.",
           },
           {
-              url: "%PUBLIC_URL%/images/4.jpg",
+              url: "/wooden-5-piece-set/images/4.jpg",
             description: "Unmatched Durability: Crafted from a single piece of 100% natural teak wood, AYZI. kitchen utensils promise a lifetime of use.",
           },
       ],
